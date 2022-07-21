1efbb089c3849fb44c3f43cac171bf16f8e364e6
kuzyi <aleckuzma@gmail.com>
2022-07-21 15:15:25 -0400

    added code for app.js and ListLostItems.jsx to fetch the list of items from our local server

--- zombiepark/src/components/ListLostItems.jsx
+++ zombiepark/src/components/ListLostItems.jsx
@@ -1,10 +1,20 @@
-// const ListLostItems = (props) => {
+const ListLostItems = (props) => {
 
-//   return (
-//     <div className="card" onClick={props.onClick}>
-//     </div>
+  return (
+    <div className="card">
+      <div className="img-wrapper">
+        <img src={props.image} alt = "no image found"></img>
+
+      </div>
+      <div className="info-wrapper flex-col">
+        <h3>{props.name}</h3>
+        <p>{props.description}</p>
+        <p>{props.date}</p>
+        
+      </div>
+    </div>
    
-//   )
-// }
+  )
+}
 
-// export default ListLostItems
+export default ListLostItems
